762abf0f0869b0f82e1647a1244c8d67fd560d86
Karoline Kuang <zk396@nyu.edu>
2018-07-12 16:33:28 +0000

    second commit

--- calc_2.cpp
+++ calc_2.cpp
@@ -1,19 +1,30 @@
 #include "std_lib_facilities.h"
 
 int main (){
-	cout << "Enter your calculations. . .\n";
-	int lval= 0, rval, res;
-	char op;
-	cin >> lval >> op >> rval;
-	switch (op) {
-		case '+': res = lval + rval;
-		break;
-		case '-': res = lval - rval;
-		break;
-		case '*': res = lval * rval;
-		break;
-		case '/': res = lval / rval;
-		break;
-	}
-	cout <<lval <<" "<< op <<" "<<rval << " = " <<res <<"\n";
+    cout << "Enter your calculations. . .\n";
+    int lval= 0, rval, res=0;
+    cin >> lval;
+    if (!cin)
+        error ("Invalid operation!");
+    for (char op= 'x'; op != '='; cin >> op){
+        if (op =='x'){
+            cin >> op;
+        }
+        cin >> rval;
+        switch (op) {
+            case '+': res = lval + rval;
+                break;
+            case '-': res = lval - rval;
+                break;
+            case '*': res = lval * rval;
+                break;
+            case '/': res = lval / rval;
+                break;
+            default : res=lval;
+        }
+        if(res!= lval){
+            lval = res;
+        }
+    }
+    cout <<res <<"\n";
 }
